62851259b9c96962eb9b6615ee03e2c7617836f6
dh <david@honisch.org>
2022-01-22 22:29:04 +0100

    update

---
+++ tests/lib/background.test.js
@@ -0,0 +1,11 @@
+// describe('ReceiveMessage', function() {
+//     describe('ping', function() {
+//         it('should return pong in response', function(done) {
+//             ReceiveMessage.ping(false, false, function(response) {
+//                 console.log(response);
+//                 expect(response).to.equal('pong');
+//                 done();
+//             });
+//         });
+//     });
+// });
